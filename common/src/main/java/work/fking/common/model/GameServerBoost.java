6be5b461712c1787b35a4fa9b0f4a6f17243ff81
Hexagon <hexagon@fking.work>
2020-11-03 19:29:25 -0300

    Enumerate game server info

---
+++ common/src/main/java/work/fking/common/model/GameServerBoost.java
@@ -0,0 +1,19 @@
+package work.fking.common.model;
+
+public enum GameServerBoost {
+    DOUBLE_PANG(1 << 1),
+    DOUBLE_EXP(1 << 2),
+    ANGEL_EVENT(1 << 3),
+    TRIPLE_EXP(1 << 4),
+    CLUB_MASTERY(1 << 7);
+
+    private final int value;
+
+    GameServerBoost(int value) {
+        this.value = value;
+    }
+
+    public int value() {
+        return value;
+    }
+}

c8aff3e07816acb639ea91eb2d5eef1e2d226a27
Hexagon <hexagon@fking.work>
2021-07-11 13:37:54 -0300

    Fix gameserver boosts

--- common/src/main/java/work/fking/pangya/common/model/GameServer.java
+++ common/src/main/java/work/fking/pangya/common/model/GameServer.java
@@ -2,6 +2,6 @@ package work.fking.pangya.common.model;
 
 import java.util.List;
 
-public record GameServer(int id, int capacity, int playerCount, String ip, int port, List<GameServerFlag> flags, List<GameServerFlag> boosts, GameServerIcon icon) {
+public record GameServer(int id, int capacity, int playerCount, String ip, int port, List<GameServerFlag> flags, List<GameServerBoost> boosts, GameServerIcon icon) {
 
 }

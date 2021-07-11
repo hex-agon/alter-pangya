2ffc667f19d7f433352210b9b9b7d4b7b0b966d2
Hexagon <hexagon@fking.work>
2021-07-11 16:15:17 -0300

    Refactor ServerList packet

--- common/src/main/java/work/fking/pangya/common/model/GameServer.java
+++ common/src/main/java/work/fking/pangya/common/model/GameServer.java
@@ -1,7 +1,41 @@
 package work.fking.pangya.common.model;
 
+import io.netty.buffer.ByteBuf;
+import work.fking.pangya.networking.protocol.ProtocolUtils;
+
 import java.util.List;
 
-public record GameServer(int id, int capacity, int playerCount, String ip, int port, List<GameServerFlag> flags, List<GameServerBoost> boosts, GameServerIcon icon) {
+public record GameServer(int id, String name, int capacity, int playerCount, String ip, int port, List<GameServerFlag> flags, List<GameServerBoost> boosts, GameServerIcon icon) {
 
+    public static GameServer of(int id, String name, int capacity, int playerCount, String ip, int port, List<GameServerFlag> flags, List<GameServerBoost> boosts, GameServerIcon icon) {
+        return new GameServer(id, name, capacity, playerCount, ip, port, flags, boosts, icon);
+    }
+
+    public void encode(ByteBuf buffer) {
+        int serverFlags = 0;
+        for (GameServerFlag flag : flags) {
+            serverFlags |= flag.value();
+        }
+
+        int serverBoosts = 0;
+        for (GameServerBoost boost : boosts) {
+            serverBoosts |= boost.value();
+        }
+
+        ProtocolUtils.writeFixedSizeString(buffer, name, 40);
+        buffer.writeIntLE(id);
+        buffer.writeIntLE(capacity);
+        buffer.writeIntLE(playerCount);
+        ProtocolUtils.writeFixedSizeString(buffer, ip, 18);
+        buffer.writeShortLE(port);
+        buffer.writeShortLE(0); // unknown
+        buffer.writeShortLE(serverFlags);
+        buffer.writeIntLE(0xFFFFFFFF); // unknown
+        buffer.writeShortLE(0xFFFF); // unknown
+        buffer.writeByte(serverBoosts); // server boosts
+        buffer.writeIntLE(0xFFFFFFFF); // unknown
+        buffer.writeShortLE(0xFFFF); // unknown
+        buffer.writeByte(0xFF); // unknown
+        buffer.writeShortLE(icon.ordinal());
+    }
 }
--- login-server/src/main/java/work/fking/pangya/login/packet/outbound/ServerListPacket.java
+++ login-server/src/main/java/work/fking/pangya/login/packet/outbound/ServerListPacket.java
@@ -1,8 +1,11 @@
 package work.fking.pangya.login.packet.outbound;
 
 import io.netty.buffer.ByteBuf;
+import work.fking.pangya.common.model.GameServer;
+import work.fking.pangya.common.model.GameServerIcon;
 import work.fking.pangya.networking.protocol.OutboundPacket;
-import work.fking.pangya.networking.protocol.ProtocolUtils;
+
+import java.util.List;
 
 public class ServerListPacket implements OutboundPacket {
 
@@ -12,48 +15,23 @@ public class ServerListPacket implements OutboundPacket {
     public void encode(ByteBuf target) {
 
         target.writeShortLE(ID);
-        target.writeByte(1); // server list size
+        List<GameServer> servers = List.of(
+                GameServer.of(
+                        20202,
+                        "Black papel",
+                        100,
+                        10,
+                        "127.0.0.1",
+                        20202,
+                        List.of(),
+                        List.of(),
+                        GameServerIcon.BLACK_PAPEL
+                )
+        );
+        target.writeByte(servers.size());
 
-        /*
-        flags
-            0x10 hides the server
-            0x80 = sort priority
-            0x800 grand prix
-
-        server icons
-            0 black papel
-            1 pippin
-            2 titan boo
-            3 dolfini
-            4 lolo
-            5 quma
-            6 tiki
-            7 cadie
-            8 cien
-
-        server boosts
-            0x2 double pang
-            0x4 double xp
-            0x8 angel event (low quit rate players 0-1%)
-            0x10 triple exp
-            0x80 club mastery (clubset points)
-        */
-
-        // repeat for each
-        ProtocolUtils.writeFixedSizeString(target, "Testing", 40);
-        target.writeIntLE(20202); // serverId
-        target.writeIntLE(1000); // capacity
-        target.writeIntLE(500); // playerCount
-        ProtocolUtils.writeFixedSizeString(target, "127.0.0.1", 18); // serverIp
-        target.writeShortLE(20202); // serverPort
-        target.writeShortLE(0); // unknown, used just to skip
-        target.writeShortLE(0); // flags?  0x10 hides the server? 0x80 = sort priority, 0x800 grand prix
-        target.writeIntLE(0xFFFFFFFF); // unknown, used just to skip
-        target.writeShortLE(0xFFFF); // unknown, used just to skip
-        target.writeByte(0x8); // server boosts
-        target.writeIntLE(0xFFFFFFFF); // unknown, used just to skip
-        target.writeShortLE(0xFFFF); // unknown, used just to skip
-        target.writeByte(0xFF); // unknown, used just to skip
-        target.writeShortLE(0); // serverIcon
+        for (GameServer server : servers) {
+            server.encode(target);
+        }
     }
 }

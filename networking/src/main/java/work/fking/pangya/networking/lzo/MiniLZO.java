e59dabf4ea0c91f6f830adf1bf6a9fdcc440a54c
Hexagon <hexagon@fking.work>
2021-08-24 23:08:20 -0300

    Refactor MiniLZO

--- networking/src/main/java/work/fking/pangya/networking/lzo/MiniLZO.java
+++ networking/src/main/java/work/fking/pangya/networking/lzo/MiniLZO.java
@@ -2,23 +2,26 @@ package work.fking.pangya.networking.lzo;
 
 public class MiniLZO {
 
-    public static final int c0_top = 1;
-    public static final int c0_try_match = 2;
-    public static final int c0_literal = 3;
-    public static final int c0_match = 4;
-    public static final int c0_m3_m4_len = 5;
-    public static final int c0_m3_m4_offset = 6;
-    public static final int c0_last = 7;
+    private static final int C_0_TOP = 1;
+    private static final int C_0_TRY_MATCH = 2;
+    private static final int C_0_LITERAL = 3;
+    private static final int C_0_MATCH = 4;
+    private static final int C_0_M_3_M_4_LEN = 5;
+    private static final int C_0_M_3_M_4_OFFSET = 6;
+    private static final int C_0_LAST = 7;
 
-    private static int _lzo1x_1_do_compress(final byte[] in, final int in_len, final byte[] out, MInt out_len, int[] dict) {
+    private MiniLZO() {
+    }
+
+    private static int _lzo1x_1_do_compress(byte[] in, int inLength, byte[] out, MInt outLength, int[] dict) {
         int ip;
         int in_base = 0;
         int out_base = 0;
         int op;
-        int in_end = in_base + in_len;
-        int ip_end = in_base + in_len - 8 - 5;
+        int in_end = in_base + inLength;
+        int ip_end = in_base + inLength - 8 - 5;
         int ii = 0;
-        int state = c0_top;
+        int state = C_0_TOP;
         op = out_base;
         ip = in_base;
 
@@ -31,68 +34,55 @@ public class MiniLZO {
         loop0:
         while (true) {
             switch (state) {
-                case c0_top:
+                case C_0_TOP:
                     dindex = 0x21 * (((in[ip + 1 + 2 + 1] & 0xff) << 6 ^ (in[ip + 1 + 1 + 1] & 0xff) << 5 ^ (in[ip + 1] & 0xff) << 5 ^ in[ip] & 0xff) >> 5) & (1 << 14) - 1;
                     m_pos = dict[dindex];
                     m_pos = ip - (ip - m_pos);
                     if (m_pos <= in_base || (m_off = ip - m_pos) <= 0 || m_off > 0xbfff) {
-                        state = c0_literal;
+                        state = C_0_LITERAL;
                         continue loop0;
                     }
                     if (m_off <= 0x0800 || in[m_pos + 3] == in[ip + 3]) {
-                        state = c0_try_match;
+                        state = C_0_TRY_MATCH;
                         continue loop0;
                     }
                     dindex = dindex & (1 << 14) - 1 & 0x7ff ^ (((1 << 14) - 1 >> 1) + 1 | 0x1f);
 
                     m_pos = dict[dindex];
                     if (m_pos < in_base || (m_off = ip - m_pos) <= 0 || m_off > 0xbfff) {
-                        state = c0_literal;
+                        state = C_0_LITERAL;
                         continue loop0;
                     }
                     if (m_off <= 0x0800 || in[m_pos + 3] == in[ip + 3]) {
-                        state = c0_try_match;
+                        state = C_0_TRY_MATCH;
                         continue loop0;
                     }
-                    state = c0_literal;
+                    state = C_0_LITERAL;
                     continue loop0;
 
-                case c0_try_match:
+                case C_0_TRY_MATCH:
                     if (in[m_pos] == in[ip] && in[m_pos + 1] == in[ip + 1]) {
                         if (in[m_pos + 2] == in[ip + 2]) {
-                            state = c0_match;
+                            state = C_0_MATCH;
                             continue loop0;
                         }
                     }
 
-                case c0_literal:
+                case C_0_LITERAL:
                     dict[dindex] = ip;
                     ip++;
                     if (ip >= ip_end) {
                         break loop0;
                     }
-                    state = c0_top;
+                    state = C_0_TOP;
                     continue loop0;
 
-                case c0_match:
+                case C_0_MATCH:
                     dict[dindex] = ip;
                     if ((ip - ii) > 0) {
                         int t = ip - ii;
 
-                        if (t <= 3) {
-                            out[op - 2] |= (byte) t;
-                        } else if (t <= 18) {
-                            out[op++] = (byte) (t - 3);
-                        } else {
-                            int tt = t - 18;
-
-                            out[op++] = 0;
-                            while (tt > 255) {
-                                tt -= 255;
-                                out[op++] = 0;
-                            }
-                            out[op++] = (byte) tt;
-                        }
+                        op = getOp(out, op, t);
                         do {
                             out[op++] = in[ii++];
                         } while (--t > 0);
@@ -111,12 +101,12 @@ public class MiniLZO {
                         } else if (m_off <= 0x4000) {
                             m_off -= 1;
                             out[op++] = (byte) (32 | (m_len - 2));
-                            state = c0_m3_m4_offset;
+                            state = C_0_M_3_M_4_OFFSET;
                             continue loop0;
                         } else {
                             m_off -= 0x4000;
                             out[op++] = (byte) (16 | ((m_off & 0x4000) >> 11) | (m_len - 2));
-                            state = c0_m3_m4_offset;
+                            state = C_0_M_3_M_4_OFFSET;
                             continue loop0;
                         }
                     } else {
@@ -134,7 +124,7 @@ public class MiniLZO {
                             } else {
                                 m_len -= 33;
                                 out[op++] = 32;
-                                state = c0_m3_m4_len;
+                                state = C_0_M_3_M_4_LEN;
                                 continue loop0;
                             }
                         } else {
@@ -154,73 +144,63 @@ public class MiniLZO {
                         out[op++] = (byte) ((m_off & 63) << 2);
                         out[op++] = (byte) (m_off >> 6);
                     }
-                    state = c0_last;
+                    state = C_0_LAST;
                     continue loop0;
-                case c0_m3_m4_len:
+                case C_0_M_3_M_4_LEN:
                     while (m_len > 255) {
                         m_len -= 255;
                         out[op++] = 0;
                     }
                     out[op++] = (byte) m_len;
 
-                case c0_m3_m4_offset:
+                case C_0_M_3_M_4_OFFSET:
                     out[op++] = (byte) ((m_off & 63) << 2);
                     out[op++] = (byte) (m_off >> 6);
-                case c0_last:
+                case C_0_LAST:
                     ii = ip;
                     if (ip >= ip_end) {
                         break loop0;
                     }
-                    state = c0_top;
+                    state = C_0_TOP;
             }
         }
 
-        out_len.v = op - out_base;
+        outLength.v = op - out_base;
         return in_end - ii;
     }
 
     /**
-     * Compress the data. Error codes would be returned (@see LZOConstants).
+     * Compress the data.
      * Compressed length is returned via out_len. Pass the integer array for
-     * dictionary(so that user can reuse the same over multiple calls. Ensure
+     * dictionary (so that user can reuse the same over multiple calls. Ensure
      * to zero out the dict contents).
      *
-     * @param in      Input byte array to be compressed
-     * @param in_len  Input length
-     * @param out     compressed output byte array. Ensure out_len =  (in_len + in_len / 16 + 64 + 3)
-     * @param out_len Compressed data length
-     * @param dict    Dictionary array. Zero out before reuse.
+     * @param in        Input byte array to be compressed
+     * @param inLength  Input length
+     * @param out       compressed output byte array. Ensure out_len =  (in_len + in_len / 16 + 64 + 3)
+     * @param outLength Compressed data length
+     * @param dict      Dictionary array. Zero out before reuse.
      */
-    public static int lzo1x_1_compress(final byte[] in, final int in_len, final byte[] out, MInt out_len, int[] dict) {
+    public static void lzo1x_1_compress(byte[] in, int inLength, byte[] out, MInt outLength, int[] dict) {
         int in_base = 0;
         int out_base = 0;
         int op = 0;
         int t;
 
-        if (in_len <= 8 + 5) {
-            t = in_len;
+        if (inLength <= 8 + 5) {
+            t = inLength;
         } else {
-            t = _lzo1x_1_do_compress(in, in_len, out, out_len, dict);
-            op += out_len.v;
+            t = _lzo1x_1_do_compress(in, inLength, out, outLength, dict);
+            op += outLength.v;
         }
 
         if (t > 0) {
-            int ii = in_base + in_len - t;
+            int ii = in_base + inLength - t;
 
             if (op == out_base && t <= 238) {
                 out[op++] = (byte) (17 + t);
-            } else if (t <= 3) {
-                out[op - 2] |= (byte) t;
-            } else if (t <= 18) {
-                out[op++] = (byte) (t - 3);
             } else {
-                int tt = t - 18;
-                out[op++] = 0;
-                while (tt > 255) {
-                    tt -= 255;
-                    out[op++] = 0;
-                }
-                out[op++] = (byte) tt;
+                op = getOp(out, op, t);
             }
             do {
                 out[op++] = in[ii++];
@@ -231,7 +211,23 @@ public class MiniLZO {
         out[op++] = 0;
         out[op++] = 0;
 
-        out_len.v = op - out_base;
-        return 0;
+        outLength.v = op - out_base;
+    }
+
+    private static int getOp(byte[] out, int op, int t) {
+        if (t <= 3) {
+            out[op - 2] |= (byte) t;
+        } else if (t <= 18) {
+            out[op++] = (byte) (t - 3);
+        } else {
+            int tt = t - 18;
+            out[op++] = 0;
+            while (tt > 255) {
+                tt -= 255;
+                out[op++] = 0;
+            }
+            out[op++] = (byte) tt;
+        }
+        return op;
     }
 }
--- networking/src/test/java/work/fking/pangya/networking/crypt/TestMiniLZO.java
+++ networking/src/test/java/work/fking/pangya/networking/crypt/TestMiniLZO.java
@@ -16,7 +16,7 @@ public class TestMiniLZO {
         byte[] bytes = ByteBufUtil.decodeHexDump("9600ffe0f50500000000");
         MInt mInt = new MInt();
         byte[] out = new byte[128];
-        int result = MiniLZO.lzo1x_1_compress(bytes, bytes.length, out, mInt, new int[32768]);
+        MiniLZO.lzo1x_1_compress(bytes, bytes.length, out, mInt, new int[32768]);
         String hexDump = ByteBufUtil.hexDump(out, 0, mInt.v);
         Assertions.assertEquals("1b9600ffe0f50500000000110000", hexDump);
     }
